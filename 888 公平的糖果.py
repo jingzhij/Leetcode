cd61a36b25df1a6f2fe884b71cb8c0cd455055bb
jingzhij <43427922+jingzhij@users.noreply.github.com>
2020-02-07 12:11:14 +1100

    Create 888 公平的糖果.py

---
+++ 888 公平的糖果.py
@@ -0,0 +1,14 @@
+### 解题思路
+注意使用集合表示两个数组，否则会超时
+
+### 代码
+
+```python3
+class Solution:
+    def fairCandySwap(self, A: List[int], B: List[int]) -> List[int]:
+        Sa, Sb = sum(A), sum(B)
+        setB = set(B)
+        for x in A:
+            if x + (Sb - Sa) / 2 in setB:
+                return [x, x + (Sb - Sa) / 2]
+            
